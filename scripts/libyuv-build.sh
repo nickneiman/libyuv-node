431a2bc200bce3dd8c54464bc4a2f2665af222d5
Nick <nickneiman@users.noreply.github.com>
2024-05-23 17:02:29 -0600

    Update libyuv-build.sh

--- scripts/libyuv-build.sh
+++ scripts/libyuv-build.sh
@@ -9,6 +9,15 @@ fi
 set -ev
 mkdir -p external
 cd external
+
+# Copy file and exit early if libyuv already installed
+if [ -n "${LIBYUV_LOCATION}" ]; then
+    echo "Found libyuv location, copying file from $LIBYUV_LOCATION..."
+    cp -r $LIBYUV_LOCATION libyuv && cp $LIBYUV_LOCATION/lib/libyuv.a . && exit 0
+else
+    echo "Did not find libyuv location, building..."
+fi
+
 if ! [ -d libyuv ]; then
     git clone https://chromium.googlesource.com/libyuv/libyuv --branch stable
 fi
@@ -28,7 +37,7 @@ else
     sed -i 's/if (JPEG_FOUND)/if (0 AND JPEG_FOUND)/' ../CMakeLists.txt
 fi
 
-cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
+cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ ..
 cmake --build . --config Release
 
 cp libyuv.a ../..
